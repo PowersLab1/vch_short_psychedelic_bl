ad86ead94b75d5a38810fb3599e44869712db29c
Max Greenwald <powerslab3@gmail.com>
2023-10-29 14:52:42 -0400

    Updates

---
+++ precache-manifest.3d0d5a79f0945c178bc7b6529ff773f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "82efd845d7bad962f038",
+    "url": "/vch_short_psychedelic_bl/static/css/main.15b15cf7.chunk.css"
+  },
+  {
+    "revision": "82efd845d7bad962f038",
+    "url": "/vch_short_psychedelic_bl/static/js/main.a7d1d07d.chunk.js"
+  },
+  {
+    "revision": "f6855b1ae6f1d2b45b56",
+    "url": "/vch_short_psychedelic_bl/static/js/runtime~main.c356c4c9.js"
+  },
+  {
+    "revision": "a4b4c706f8375feb67ea",
+    "url": "/vch_short_psychedelic_bl/static/js/2.173aec18.chunk.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/vch_short_psychedelic_bl/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "b070f1811207348f54b4988fc14bc950",
+    "url": "/vch_short_psychedelic_bl/static/media/rating_keydown_1.b070f181.png"
+  },
+  {
+    "revision": "14052abb8ad3bf096d71788ba2e2ace2",
+    "url": "/vch_short_psychedelic_bl/static/media/rating_keydown_2.14052abb.png"
+  },
+  {
+    "revision": "bc40f6358057323efebef6ea2db6cb59",
+    "url": "/vch_short_psychedelic_bl/static/media/rating_keydown_3.bc40f635.png"
+  },
+  {
+    "revision": "852779dc3fb4af1fd92cb2611fe82806",
+    "url": "/vch_short_psychedelic_bl/static/media/rating_keydown_4.852779dc.png"
+  },
+  {
+    "revision": "ae6e6c1fe3fef7ab5aa19a93442d23d3",
+    "url": "/vch_short_psychedelic_bl/static/media/rating_keydown_5.ae6e6c1f.png"
+  },
+  {
+    "revision": "d6296a42a6add699cf7557f95312adbb",
+    "url": "/vch_short_psychedelic_bl/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/vch_short_psychedelic_bl/precache-manifest.3d0d5a79f0945c178bc7b6529ff773f2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/vch_short_psychedelic_bl/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
